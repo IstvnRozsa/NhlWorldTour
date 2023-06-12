c9127e15a025f7fed133d3d549b20044b2250131
pistirozsa <pisti.rzsa@gmail.com>
2023-06-12 11:02:44 +0200

    remove the old dataset

--- app.py
+++ app.py
@@ -8,14 +8,9 @@ app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 @app.route('/')
 def hello_world():  # put application's code here
-
-    with open("data/&season=20142015.json", 'r') as file:
-        json_data = json.load(file)
-    season_2014 = json.dumps(json_data)
-
     seasons = json.dumps(get_data())
 
-    return render_template('index.html', season_2014=season_2014, seasons=seasons)
+    return render_template('index.html', seasons=seasons)
 
 
 if __name__ == '__main__':
