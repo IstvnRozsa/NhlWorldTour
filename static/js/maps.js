1c8f6705d96eb13f48183b5b0d5b97d1fff831c2
pistirozsa <pisti.rzsa@gmail.com>
2023-06-12 11:03:41 +0200

    projection and onhover function on the map

--- static/js/maps.js
+++ static/js/maps.js
@@ -1,41 +1,64 @@
+function drawMap() {
+
 // Set the width and height of the SVG container
-const width = 800;
-const height = 600;
-// HALLOOO Canada is not in the US
+    const width = 800;
+    const height = 600;
 // Create an SVG container
-const svg = d3.select("#teams_map")
-  .attr("width", width)
-  .attr("height", height);
+    const svg = d3.select("#teams_map")
+        .attr("width", width)
+        .attr("height", height);
 
 // Load the map data (e.g., GeoJSON)
-d3.json("https://raw.githubusercontent.com/iamspruce/intro-d3/main/data/countries-110m.geojson").then(function (mapData) {
-  // Create a projection
-  const projection = d3.geoMercator()
-    .fitSize([width, height], mapData);
+    d3.json("https://raw.githubusercontent.com/iamspruce/intro-d3/main/data/countries-110m.geojson").then(function (mapData) {
+        // Create a projection
 
-  // Create a path generator
-  const path = d3.geoPath()
-    .projection(projection);
+        let projection = d3.geoMercator()
+            .center([-80, 45])
+            .scale(300)
+            .translate([width / 2, (height - 100) /2]);
 
-  // Draw the map
-  svg.selectAll("path")
-    .data(mapData.features)
-    .enter()
-    .append("path")
-    .attr("d", path)
-    .attr("fill", "lightgray");
+        // Create a path generator
+        const path = d3.geoPath()
+            .projection(projection);
 
-  // Draw city points
-  svg.selectAll("circle")
-    .data(season_2014.teams)
-    .enter()
-    .append("circle")
-    .attr("cx", function (d) {
-      return projection([d.longitude, d.latitude])[0];
-    })
-    .attr("cy", function (d) {
-      return projection([d.longitude, d.latitude])[1];
-    })
-    .attr("r", 5)
-    .attr("fill", "red");
-});
+        // Draw the map
+        svg.selectAll("path")
+            .data(mapData.features)
+            .enter()
+            .append("path")
+            .attr("d", path)
+            .attr("fill", "lightgray");
+
+        // Draw city points
+        svg.selectAll("circle")
+            .data(selectedSeason.teams)
+            .enter()
+            .append("circle")
+            .attr("cx", function (d) {
+                return projection([d.longitude, d.latitude])[0];
+            })
+            .attr("cy", function (d) {
+                return projection([d.longitude, d.latitude])[1];
+            })
+            .attr("r", 4)
+            .attr("fill", "red")
+            .attr("id", function (d) {
+                return d.name;
+            })
+            .attr("class", function (d) {
+                return d.locationName;
+            })
+            .on("mouseover", function (d) {
+                // Show city name label
+                //console.log(d);
+                //console.log(d.target.id);
+                //console.log(d.target.classList[0] + " " + d.target.classList[1]);
+                selectTeam(d.target.id);
+            })
+
+
+    });
+    console.log("Draw Map");
+}
+
+drawMap();
