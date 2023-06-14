564a4ebd2dfdb1b97532251d1ccf7e6030b8569c
Michaela Kučerová <michaela.kucerova@quanti.cz>
2023-06-14 14:01:06 +0200

    selecting data for both teams

--- static/js/selectTeam.js
+++ static/js/selectTeam.js
@@ -1,4 +1,3 @@
-
 let combobox_teams = d3.select('#combobox_teams');
 
 combobox_teams.selectAll('option')
@@ -8,23 +7,31 @@ combobox_teams.selectAll('option')
     .text(function (d) {
         return d['name'];
     });
-function selectStatisticalData(objects, name) {
-    let data = [];
+
+function selectTeamData(objects, name) {
+    let dataTeam1 = [];
+    let dataTeam2 = [];
     for (let i = 0; i < objects.length; i++) {
-        if (objects[i].abbreviation === previousTeamId) {
-            data.push(objects[i].teamStats[0].splits[0].stat[name]);
-            // return objects[i];
+        if (objects[i].name === name) {
+            console.log("obj: ");
+            console.log(objects[i]);
+            dataTeam2.push(objects[i].teamStats[0].splits[0].stat);
+        } else if (objects[i].abbreviation === previousTeamId) {
+            dataTeam1.push(objects[i].teamStats[0].splits[0].stat);
         }
     }
-    console.log(data);
-    return data;
+    console.log(previousTeamId);
+    console.log(dataTeam1);
+    console.log(dataTeam2);
+    return [dataTeam1, dataTeam2];
     // return null; // Return null if no match found
 }
 
 combobox_teams.on('change', function () {
     // Get the selected value
-    let selectedStat = d3.select(this).property('value');
-    selectedStatistic = selectStatisticalData(teams, selectedStat);
-    console.log("stats" + selectedStat, selectedStatistic);
-    updateLinePlot(selectedStatistic);
+    let selected = d3.select(this).property('value');
+    let selectedTeams = selectTeamData(teams, selected);
+    console.log("stats" + selected + ": " + selectedTeams[0]);
+    console.log(selectedTeams[0])
+    updateLinePlot(selectedTeams);
 });
